1ba34d2268b1d1eb432187e16e3d559a9c83d91c
Veronica Putri Anggraini <veronicaputri497@gmail.com>
2023-02-24 22:29:01 +0700

    add retrofit

---
+++ app/src/main/java/com/veroanggra/movieplayground/data/MovieResponse.kt
@@ -0,0 +1,25 @@
+package com.veroanggra.movieplayground.data
+
+data class MovieResponse(
+    val page: Int,
+    val totalPages: Int,
+    val results: List<ResultsItem>,
+    val totalResults: Int
+)
+
+data class ResultsItem(
+    val overview: String,
+    val originalLanguage: String,
+    val originalTitle: String,
+    val video: Boolean,
+    val title: String,
+    val genreIds: List<Int>,
+    val posterPath: String,
+    val backdropPath: String,
+    val releaseDate: String,
+    val popularity: String,
+    val voteAverage: String,
+    val id: Int,
+    val adult: Boolean,
+    val voteCount: Int
+)
