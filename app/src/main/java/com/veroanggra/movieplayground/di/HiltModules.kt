e34449a9c7cf160532f9ebf0d5ebcf5e88d552db
Veronica Putri Anggraini <veronicaputri497@gmail.com>
2023-02-27 00:38:01 +0700

    add provider

--- app/src/main/java/com/veroanggra/movieplayground/di/HiltModules.kt
+++ app/src/main/java/com/veroanggra/movieplayground/di/HiltModules.kt
@@ -1,10 +1,21 @@
 package com.veroanggra.movieplayground.di
 
+import com.veroanggra.movieplayground.repository.remote.MovieService
+import com.veroanggra.movieplayground.utils.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 @InstallIn(SingletonComponent::class)
 @Module
 object HiltModules {
+    @Provides
+    fun provideRetrofitInterface(): MovieService {
+        return Retrofit.Builder().baseUrl(Constants.BASE_URL).addConverterFactory(
+            GsonConverterFactory.create()
+        ).build().create(MovieService::class.java)
+    }
 }
