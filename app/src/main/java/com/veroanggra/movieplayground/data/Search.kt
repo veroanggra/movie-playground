8cc1b1d818cfb0ace866958c89b6a298db4a9c0c
Veronica Putri Anggraini <veronicaputri497@gmail.com>
2023-02-27 00:28:49 +0700

    add movie response

--- app/src/main/java/com/veroanggra/movieplayground/data/Search.kt
+++ app/src/main/java/com/veroanggra/movieplayground/data/Search.kt
@@ -1,4 +1,9 @@
 package com.veroanggra.movieplayground.data
 
-class Search {
-}
+data class Search(
+    val Poster: String,
+    val Title: String,
+    val Type: String,
+    val Year: String,
+    val imdbID: String
+)
