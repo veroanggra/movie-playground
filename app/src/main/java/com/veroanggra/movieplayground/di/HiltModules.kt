24c91d3a163274ac45446174e15605a78eb13aca
racheldhiamaharni <dhiarachella@gmail.com>
2023-03-01 21:14:49 +0700

    paging

--- app/src/main/java/com/veroanggra/movieplayground/di/HiltModules.kt
+++ app/src/main/java/com/veroanggra/movieplayground/di/HiltModules.kt
@@ -1,10 +1,22 @@
 package com.veroanggra.movieplayground.di
 
+import com.veroanggra.movieplayground.repository.remote.MovieService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 @InstallIn(SingletonComponent::class)
 @Module
+
 object HiltModules {
+    @Provides
+    fun provideRetrofitInterface(): MovieService {
+        return Retrofit.Builder()
+            .baseUrl("").addConverterFactory(GsonConverterFactory.create())
+            .build().create(MovieService::class.java)
+    }
 }
--- app/src/main/java/com/veroanggra/movieplayground/repository/remote/MovieService.kt
+++ app/src/main/java/com/veroanggra/movieplayground/repository/remote/MovieService.kt
@@ -1,10 +1,12 @@
 package com.veroanggra.movieplayground.repository.remote
 
 import com.veroanggra.movieplayground.data.MovieResponse
+import dagger.hilt.android.AndroidEntryPoint
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
-
+// biar akses hilt
+@AndroidEntryPoint
 interface MovieService {
     @GET("/movie/popular")
     suspend fun getDiscover(
